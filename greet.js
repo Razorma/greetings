f7814661bf10702b4775337a6b9df55013ecabfa
bheka <bheka.bj@gmail.com>
2023-04-18 16:50:15 +0200

    factory function done

--- greet.js
+++ greet.js
@@ -1,3 +1,25 @@
-function greet(){
-    
+function greeting(){
+    let name = '';
+    function getName(passedName){
+        name = passedName;
+    }
+    function greetName(){
+        return "Hello, " + name
+    }
+    return{
+        getName,
+        greetName,
+    }
 }
+const greet = greeting()
+greet.getName("mama")
+console.log(greet.greetName())
+const greetButton = document.querySelector(".greetNameButton")
+const greetingElement = document.querySelector(".greeting");
+const greetNamePassed = document.querySelector(".name-area")
+ 
+greetButton.addEventListener("click",function(){
+    greet.getName(greetNamePassed.value)
+    greetingElement.innerHTML = greet.greetName()
+    greetNamePassed.value = '';
+})
